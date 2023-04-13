5ae028c291fda792988591a141ac30623fca15b9
bi3mer <colan@comcast.net>
2023-04-13 13:25:36 -0400

    basic set up

---
+++ webpack.prod.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  output: {
+  filename: "bundle.js"
+  },
+  optimization: {
+    chunkIds: false,
+  },
+  resolve: {
+  // Add `.ts` and `.tsx` as a resolvable extension.
+  extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+  rules: [
+    // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+    {
+    test: /\.tsx?$/, 
+    exclude: '/node_modules/',
+    loader: "ts-loader" }
+  ]
+  }
+};
